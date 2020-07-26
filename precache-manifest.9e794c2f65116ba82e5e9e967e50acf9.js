e93250fd00c6fa5db732e696ace72e20d9a4e294
Abhayks2808 <Abhayks2808@gmail.com>
2020-07-26 20:24:09 +0530

    Updates

---
+++ precache-manifest.9e794c2f65116ba82e5e9e967e50acf9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde23c1806a7167870a208292b821f94",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "26fc721f17a7f4863222",
+    "url": "/monster-rolodex/static/css/main.5edea523.chunk.css"
+  },
+  {
+    "revision": "1d03717090029279e21b",
+    "url": "/monster-rolodex/static/js/2.b2d15453.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.b2d15453.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26fc721f17a7f4863222",
+    "url": "/monster-rolodex/static/js/main.8baceaf8.chunk.js"
+  },
+  {
+    "revision": "085e5ddfce17ca69148e",
+    "url": "/monster-rolodex/static/js/runtime-main.c0ab3219.js"
+  }
+]);
